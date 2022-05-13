e65145afee5ed5f773c33a83886cc61bfc0b2214
HoPGoldy <hpgless@outlook.com>
2022-05-13 16:17:13 +0800

    feat: 自动部署

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import styleImport from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/realworld-vue3-antd/',
     resolve: {
         alias: {
             '@': resolve(__dirname, './src')
